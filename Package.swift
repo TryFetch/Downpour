2ca66a58644580451db08418968a88c1712a2f36
Jacob Williams <ponyboy47@gmail.com>
2017-03-29 14:06:35 -0600

    vdka/JSON is only needed for linux

--- Package.swift
+++ Package.swift
@@ -1,11 +1,14 @@
 import PackageDescription
 
+var dependencies: [Package.Dependency] = [.Package(url: "https://github.com/Ponyboy47/PathKit.git", majorVersion: 0, minor: 8)]
+
+#if os(Linux)
+dependencies.append(.Package(url: "https://github.com/vdka/JSON.git", majorVersion: 0, minor: 16))
+#endif
+
 let package = Package(
   name: "Downpour",
-  dependencies: [
-      .Package(url: "https://github.com/Ponyboy47/PathKit.git", majorVersion: 0, minor: 8),
-      .Package(url: "https://github.com/vdka/JSON.git", majorVersion: 0, minor: 16)
-  ],
+  dependencies: dependencies,
   exclude: [
       "Tests"
   ]
