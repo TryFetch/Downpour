0f82489d8e791d48c705874ef218d69e6a4027f0
Jacob Williams <ponyboy47@gmail.com>
2019-07-02 14:07:18 -0600

    Bump some version stuff and use a better travis xcode image

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
       .library(name: "Downpour", targets: ["Downpour"])
   ],
   dependencies: [
-      .package(url: "https://github.com/Ponyboy47/TrailBlazer.git", from: "0.15.0"),
-      .package(url: "https://github.com/Ponyboy47/SwiftShell.git", from: "4.2.0")
+      .package(url: "https://github.com/Ponyboy47/TrailBlazer.git", from: "0.16.0"),
+      .package(url: "https://github.com/kareman/SwiftShell.git", from: "5.0.0")
   ],
   targets: [
       .target(
